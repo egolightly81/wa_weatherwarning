34fee1e86bff9764c3a863ab8838c6f1a733b062
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-06-05 16:36:39 +0800

    parse xml data

--- JS/script.js
+++ JS/script.js
@@ -11,12 +11,12 @@ fetch('https://rss.weatherzone.com.au/?u=12994-1285&lt=aploc&lc=13896&obs=1&fc=1
     items.forEach(item => {
       const title = item.querySelector('title').textContent;
       const link = item.querySelector('link').textContent;
-      const description = item.querySelector('description').textContent;
+      const pubDate = item.querySelector('pubDate').textContent;
 
       const article = document.createElement('article');
       article.innerHTML = `
         <h2><a href="${link}" target="_blank">${title}</a></h2>
-        <p>${description}</p>
+        <p>Published: ${pubDate}</p>
       `;
       feedContainer.appendChild(article);
     });
